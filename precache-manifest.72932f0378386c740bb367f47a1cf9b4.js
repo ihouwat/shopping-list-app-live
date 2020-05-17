5537881651a6b27ee14b63161e7190d6e8f42673
ihouwat <houwat@gmail.com>
2020-05-17 14:10:31 -0400

    Updates

---
+++ precache-manifest.72932f0378386c740bb367f47a1cf9b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8fe4f87db78db43a7831497b1015cd2",
+    "url": "/shopping-list-react-app-complete/index.html"
+  },
+  {
+    "revision": "2b1b131b67087d961a4e",
+    "url": "/shopping-list-react-app-complete/static/css/main.216097bf.chunk.css"
+  },
+  {
+    "revision": "8c4ac4b5b2ff337ace3c",
+    "url": "/shopping-list-react-app-complete/static/js/2.1dc84b4f.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/shopping-list-react-app-complete/static/js/2.1dc84b4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b1b131b67087d961a4e",
+    "url": "/shopping-list-react-app-complete/static/js/main.4a054ebb.chunk.js"
+  },
+  {
+    "revision": "bb26e95f804992ef1691",
+    "url": "/shopping-list-react-app-complete/static/js/runtime-main.2db8b831.js"
+  }
+]);
